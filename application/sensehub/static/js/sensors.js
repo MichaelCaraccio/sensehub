d6601b779b8da18dd528cc482e99e1a714354579
Nicolas Huguenin <nichuguen@gmail.com>
2017-09-07 18:07:24 +0200

    updated sensor page

--- application/sensehub/static/js/sensors.js
+++ application/sensehub/static/js/sensors.js
@@ -4,6 +4,26 @@ var app = new Vue({
   el: '#app',
   delimiters: ['[[', ']]'],
   data: {
-    message: 'Hello Vue!'
+    sensors: []
   }
 })
+
+window.onload = init
+
+function init()
+{
+    fetch('/api/sensors/',{
+        method: "GET",
+        credentials: 'same-origin', // this allows the credentials to be kept (logged in user)
+})
+    .then((resp) => resp.json())
+    .then(function(data) {
+        if (data.status === "ok")
+        {
+            data.message.forEach(function(sensor){
+                console.log(sensor);
+                app.sensors.push(sensor);
+            });
+        }
+    }).catch((error) => console.log("Error getting the data: "+error));
+}
