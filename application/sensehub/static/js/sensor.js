4944380864c8ddec870ce8f6892a2a1c661c73d9
Michael Caraccio <michael.caraccio@gmail.com>
2017-09-08 01:10:06 +0200

    add page for sensor and get value

---
+++ application/sensehub/static/js/sensor.js
@@ -0,0 +1,32 @@
+'use strict';
+
+var app = new Vue({
+  el: '#app',
+  delimiters: ['[[', ']]'],
+  data: {
+    sensor: []
+  }
+})
+
+window.onload = init
+
+function init()
+{
+    app.loading = true;
+    fetch('/api/sensor/' + sensor_id, {
+        method: "GET",
+        credentials: 'same-origin', // this allows the credentials to be kept (logged in user)
+    })
+    .then((resp) => resp.json())
+    .then(function(data) {
+        if (data.status === "ok") {
+            data.message.forEach(function(sensor){
+                sensor.url = '/sensor/'+ sensor.id;
+                console.log(sensor);
+                app.sensor = sensor;
+
+            });
+            app.loading = false;
+        }
+    }).catch((error) => console.log("Error getting the data: "+error));
+}
